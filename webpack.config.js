c1d042e88f684829c7ba544407be45129ac2dc38
widrelk <widrelk@gmail.com>
2021-07-29 02:50:54 +0700

    Add webpack 4 to the project

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+module.exports = {
+  entry: { main: './src/index.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  node: {
+    fs: "empty"
+  }
+}
