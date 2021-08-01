05de9470b556962b5980d60350cfa956cd6bd18e
widrelk <widrelk@gmail.com>
2021-08-01 13:35:57 +0700

    Remove even more files and move to webpack 4
    - Finished with tidying up library files. Now no more unused code,
    such as old html writer and so on.
    Still some space fore improvements.
    
    - Webpack 4 is now working correctly because fs is not present
    
    - Version updated to 0.6.0

--- src/xml/index.js
+++ src/xml/index.js
@@ -4,4 +4,3 @@ exports.Element = nodes.Element;
 exports.element = nodes.element;
 exports.text = nodes.text;
 exports.readString = require("./reader").readString;
-exports.writeString = require("./writer").writeString;
